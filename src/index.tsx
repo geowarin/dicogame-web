da91c71e5a44625dbf14ce3e14c7b92c822574fc
geowarin <geowarin@gmail.com>
2015-08-30 22:13:56 +0200

    Update 2015-08-30T20:13:55.697Z

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-/// <reference path="./typings/tsd.d.ts" />
-
-import React = require('react');
-import App from './App';
-
-React.render(<App />, document.getElementById('app'));
--- src/typings/tsd.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="react/react.d.ts" />
-/// <reference path="axios/axios.d.ts" />
-/// <reference path="es6-promise/es6-promise.d.ts" />
