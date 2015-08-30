341e322903afebecb8fc10d9a3ef89df67699114
geowarin <geowarin@gmail.com>
2015-08-30 22:11:31 +0200

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 
-gulp.task('deploy', function() {
+gulp.task('copy:data', function() {
   return gulp.src('./data/**/*')
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('deploy', function() {
+gulp.task('deploy', ['copy:data'], function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
